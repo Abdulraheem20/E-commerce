ecf33ecd1c43bce02eb52a09c38661a9ada8cdf8
Abdulraheem <raimitaiwo45@gmail.com>
2022-09-29 05:12:04 +0100

    new commit

--- script.js
+++ script.js
@@ -6,16 +6,43 @@ console.log(smallImages);
 for(let i = 0; i < smallImages.length; i++ ){
     smallImages[0].classList.add('opaRm')
     smallImages[i].addEventListener('click', (e)=>{
+        e.preventDefault()
         bigImage.setAttribute('src', smallImages[i].src)
-        if(smallImages[i].click){
+        bigImage.style.width = '400px'
+        
+        if(smallImages[i].src == bigImage.src){
             smallImages[i].classList.add('opaRm')
-        }else{
-            smallImages[i].classList.remove('opaRm')
+        } else if(smallImages[i].src == bigImage.src){
+            smallImages[i].classList.add('opa')
+           
         }
-    })
-}
+        // if(smallImages[i].classList == 'op')
 
-// let add = document.querySelector('.plusMinus .plus button');
-// let minus = document.querySelector('.plusMinus .minus button');
-// let display = document.querySelector('..plusMinus span');
-// let quantity = 0;
+     })
+    //  smallImages[i].style.opacity = '1'
+    
+} 
+
+let add = document.querySelector('.plus h1');
+let minus = document.querySelector('.plusMinus .minus h1');
+let display = document.querySelector('.plusMinus span');
+let displayValue = 0;
+
+
+add.addEventListener('click', (e)=>{
+    display.textContent = displayValue+=1
+});
+
+;
+minus.addEventListener('click', ()=>{
+    let min = 0
+    if (displayValue >  0){
+        display.textContent = displayValue-=1
+    } else if (displayValue ==  0){
+        display.textContent = 0
+    }
+})
+ const addToCart = document.querySelector('.addToCart');
+ addToCart.addEventListener('click', ()=>{
+    
+ })
